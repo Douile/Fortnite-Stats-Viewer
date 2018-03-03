5470f7848ab971fb641463bd6252c480a76117f7
Tom <25043847+Douile@users.noreply.github.com>
2018-03-03 22:33:15 +0000

    Update stats-viewer.js

--- docs/stats-viewer.js
+++ docs/stats-viewer.js
@@ -3,6 +3,24 @@
 var StatsViewer = {
   init: function() {
     this.key = document.querySelector("#keyinput");
+    this.name = document.querySelector("#nameinput");
+    this.output = document.querySelector("#outputdiv");
+    this.send = document.querySelector("#sendinput");
+    
+    this.send.addEventListener("click",this.sendListener);
   },
-  body: document.body
+  body: document.body,
+  sendListener: function(e) {
+    var key = this.key.value;
+    var name = this.name.value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","https://fortnite.y3n.co/v2/player/"+name);
+    xhr.addEventListener("load",this.data);
+    xhr.send();
+  },
+  data: function(e) {
+    console.log(e);
+  }
 };
+
+StatsViewer.init();
